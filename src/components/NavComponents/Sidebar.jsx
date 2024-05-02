7081981a61e47929b820dacadcdbd50042f204cb
Black Bobs <ayodejiayodele350@gmail.com>
2024-05-02 18:23:16 +0100

    feat: added the news api

--- src/components/NavComponents/Sidebar.jsx
+++ src/components/NavComponents/Sidebar.jsx
@@ -42,14 +42,12 @@ const Sidebar = () => {
     <aside className={`absolute top-0 ${showMenu ? 'left-0' : '-left-full'} w-60 h-screen bg-dark border-r border-r-primary z-10 transition-all duration-1000`}>
         <div className="flex items-center justify-between p-2">
         <h1 className="text-xl font-bold italic text-white">Mage</h1>
-        <button onClick={handleCloseMenu} className="border-none outline-none bg-transparent">
-        <IoClose onClick={handleCloseMenu} className="text-light cursor-pointer" size={30}/>
-        </button>
+        <IoClose onClick={() => handleCloseMenu} className="text-light cursor-pointer" size={30}/>
         </div>
         <div className="flex flex-col gap-5 my-3 p-3">
             {
                 links.map((link, i) => (
-                    <Link onClick={handleCloseMenu} className="no-underline flex items-center gap-2 text-light cursor-pointer" href={link.route} key={i}>
+                    <Link onClick={() => handleCloseMenu} className="no-underline flex items-center gap-2 text-light cursor-pointer" href={link.route} key={i}>
                         <span>{link.icon}</span>
                         <p className="capitalize font-semibold">{link.name}</p>
                     </Link>
--- src/store/news.js
+++ src/store/news.js
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 
 const fetcher = async () => {
-    const response = await fetch('https://newsdata.io/api/1/news?apikey=pub_43173dc8326317d4ee688c2fd69ebc5ccd3d0&q=pizza')
+    const response = await fetch('https://newsdata.io/api/1/news?apikey=pub_43173dc8326317d4ee688c2fd69ebc5ccd3d0&q=art')
     const data = await response.json()
     return data
 }
